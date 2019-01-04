1e37ea6a53eab342589c1335aa4ffae520e83289
John Condon <jackcondon@Johns-MacBook-Pro.local>
2019-01-04 15:24:34 -0500

    starting second screen

--- js/college.js
+++ js/college.js
@@ -1,46 +1,29 @@
 
 
-d3.json('data/us_region.json').then(function(data) {
+d3.json('data/us_features.json').then(function(data) {
 
+   var svg = d3.select("#content").append("svg");
+   var projection = d3.geoAlbersUsa();
+   var path = d3.geoPath().projection(projection);
 
-
-
-
-// var width = 500;
-// var height = 200;
-    
-
- var svg = d3.select("#content").append("svg");
-
- var projection = d3.geoAlbersUsa();
-    
- var path = d3.geoPath().projection(projection);
-
-
- data.geometries.forEach(element => {
+   data.features.forEach(element => {
    
-   svg.append("path")
-   .attr("d", path(element))
-   .attr("stroke", "black")
-   .attr("fill", "#d9e2df")
-   .on("mouseover", function(){
-      d3.select(this)
-      .attr("fill", "orange")})
-   .on("mouseout", function(){
-      d3.select(this)
+      console.log(element.properties['name']);
+
+      svg.append("path")
+      .attr("d", path(element.geometry))
+      .attr("stroke", "black")
       .attr("fill", "#d9e2df")
-       })
-
+      .on("mouseover", function(){
+         d3.select(this)
+         .attr("fill", "#82edcd")
+         d3.select('.region').text(element.properties['name'])
+      })
+      .on("mouseout", function(){
+         d3.select(this)
+         .attr("fill", "#d9e2df")
+         d3.select('.region').text('')
+      });
+   });
 });
-
-   
-
-
-});
-   
-
-
-
-
-
 
